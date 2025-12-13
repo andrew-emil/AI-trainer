5a2a3239af58df34398d6cc90fdfc72b501fcf0f
adhm-dell <adhmdoma11@gmail.com>
2025-12-13 06:14:10 +0200

    making progress service and the migraition files for it

---
+++ services/laravel/progress/database/migrations/2025_12_13_033149_create_workout_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workout_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('workout_id');
+            $table->string('exercise_name');
+            $table->json('exercises_performed');
+            $table->float('calories_burned')->nullable();
+            $table->float('duration_minutes');
+            $table->date('workout_date');
+            $table->text('notes')->nullable();
+            $table->string('workout_type')->nullable();
+            $table->float('volume')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workout_logs');
+    }
+};
